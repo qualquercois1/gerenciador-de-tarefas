391f8c6bcb84c15a6268da6cdc6b0095978e2ce8
qualquercois1 <cassiodossantosrodrigues@gmail.com>
2025-08-26 23:18:52 -0300

    frontend adicionado

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+            {
+                path: 'home',
+                component: HomeComponent
+            },
+            {
+                path: '',
+                redirectTo: 'home',
+                pathMatch: 'full'
+            }
+        ]
+    }
+];
